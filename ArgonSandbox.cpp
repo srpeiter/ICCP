1d5f51ff1b6605ee704112bc33e5d670b1343be8
IRiViI <rckvnk@gmail.com>
2015-02-18 21:45:32 +0000

    The file now has function to calculate the potential energy and kinetic energy, the energy is more or less constant

--- ArgonSandbox.cpp
+++ ArgonSandbox.cpp
@@ -8,13 +8,12 @@ using namespace std;
 
 /* 
 This is a kind or remake of Argonsimulation.cpp.
-Out of hopelessness, I created a new file which is more simple such that I can investigate the problem more isolated.
+Out of hopelessness, I created a new file that startes simpler such that I can investigate the problem more isolated.
 */
 
 /* 
 NOTES:
-The function Distance is not pralocated correctly
-The functions Distance and force do not work yet
+Life is awesome
 */
 
 
@@ -28,14 +27,18 @@ _________________________________________________________
 
 int N;			// Number of particles
 int Run; 		// Number of simulations rounds
+int run;		// Round number
+int runtemp = 0;	// Used for the display function 
 double dt;		// discrete time step
-const int dim = 3;	// Number of dimensions
+const int dim = 3;	// Number of dimension
+double EK;		// Total kinetic energy
+double EP;		// Total potential energy
 double *pos[dim];	// Position array x,y,z
 double *vel[dim];	// Velocity array x,y,z
-double *forc[dim];	// Force array x,y,z
+double *force[dim];	// Force array x,y,z
 
 double *dist[dim];	// A temperary matrix used for distance calculations
-//double Dist2[N];	// A temperary vector also used for distane calculations
+
 
 /*
 _________________________________________________________
@@ -49,8 +52,13 @@ void Make_array();
 void Parameters();
 void Initiate_Position();
 void Initiate_Velocity();
+void Update_position();
+void Update_velocity();
+void Update_force();
 void Display();
-void Distance();
+void Calculate_Potential_Energy();
+void Calculate_Kinetic_Energy();
+void Energy_Correction();
 
 /*
 _________________________________________________________
@@ -76,30 +84,33 @@ _________________________________________________________
 void Make_array(int argc, char* argv[])
 {
 int i;
-if (argc > 2){
-N=atoi(argv[1]); 
+// Matrix preallocating
 for (i = 0; i< dim; i++){
 pos[i]= new double[N] (); 
 vel[i]= new double[N] (); 
-forc[i]= new double[N] ();
+force[i]= new double[N] ();
 dist[i] = new double[N] ();
-} } 
+} 
+// Vector preallocating
 }
 
 // ________ Process parameters ________ \\
 
 void Parameters(int argc, char* argv[]){
 if (argc < 2){
-printf("Error: Give the number of particles N followed by the number of runs. \nExample: './ArgonSandbox 9 10', meaning 9 particles and 10 runs\n");}
-else 
+printf("Error: Give the number of particles N followed by the number of runs. \nExample: './ArgonSandbox 9 10 0.001', meaning 9 particles and 10 runs 1\n");}
+else{
+N = atoi(argv[1]);
 Run = atoi(argv[2]); 
+dt = 0.001;
+}
 }
 
 // ________ Initiate particles position ________ \\
 
 void Initiate_Position(){
 for (int n=0; n<N; ++n){
-pos[0][n] = n;	// Let's start easy
+pos[0][n] = 1.1 * n;	// Let's start easy
 pos[1][n] = 0;
 pos[2][n] = 0;
 }
@@ -110,7 +121,7 @@ pos[2][n] = 0;
 void Initiate_Velocity(){
 for (int n=0; n<N; ++n){
 vel[0][n] = 0;
-vel[1][n] = n;
+vel[1][n] = 0;
 vel[2][n] = 0;
 }
 }
@@ -128,7 +139,7 @@ _________________________________________________________
 void Update_position(){
 for (int n = 0; n < N; ++n){
 for (int i = 0; i < dim; ++i){		
-pos[i][n] = pos[i][n] + vel[i][n] *dt;
+pos[i][n] = pos[i][n] + vel[i][n] *dt;		// x(i) = x(i-1) + v(i)*dt
 }}
 }
 
@@ -138,39 +149,79 @@ pos[i][n] = pos[i][n] + vel[i][n] *dt;
 void Update_velocity(){
 for (int n = 0; n < N; ++n){
 for (int i = 0; i < dim; ++i){		
-vel[i][n] = vel[i][n] + forc[i][n] *dt;
+vel[i][n] = vel[i][n] + force[i][n] *dt;	// v(i) = v(i-1) + f(i)*dt
 }}
-}
-
-// ________ Distance ________ \\
-
-
-void Distance(int& n){
-cout << "test" << endl;
-/*
-for (int m = 0; m < N; ++m){
-for (int d = 0; d < dim; ++d) {		
-dist[d][m] = pos[d][m] - pos[d][n];
-}
-Dist2[m] = pow(dist[0][m],2) + pow(dist[1][m],2) + pow(dist[2][m],2)
-}
-*/
 }
 
 // ________ Update particles force ________ \\
 
 
 void Update_force(){
-int i,n;
-for (n = 0; n < N; ++n){
-for (i = 0; i < dim; ++i) {
-Distance(n);
-		
-//forc[i][n] = forc[i][n] + forc[i][n] *dt;
-}}
+double Dist2;
+int d;
+for (int n = 0; n < N; ++n){
+for (d = 0; d < dim; ++d)
+force[d][n] = 0; 			// Reset the force
+for (int m = 0; m < N; ++m){
+if (n !=m){				// Only calculate between different 
+					// particles
+Dist2 = 0;				// Reset "Distance squared"
+for (d = 0; d < dim; ++d){
+dist[d][m] = pos[d][m] - pos[d][n];	// Calculate the distance in x,y or z
+Dist2 += pow(dist[d][m],2);		// Calculate the distance squared
+}
+for (d = 0; d < dim; ++d)		// Calculate the force
+force[d][n] += 24 * (-2*pow(Dist2,-7) + pow(Dist2,-4)) * dist[d][m];
+} } }
 }
 
+/*
+_________________________________________________________
 
+ ------------ Energy related functions ----------------- 
+_________________________________________________________
+
+*/
+
+// ________ Calculate Potential Energy ________ \\
+
+// Note: It is more optimal to calculate the force and potential energy in one function
+
+void Calculate_Potential_Energy(){
+double Dist2;
+EP = 0; 				// Reset total potential energy parameter
+for (int n = 0; n < N; ++n){
+for (int d = 0; d < dim; ++d)
+for (int m = n+1; m < N; ++m){		// Do not count double the potential energy
+Dist2 =0;				// Reset distance squared
+for (int d = 0; d < dim; ++d){
+dist[d][m] = pos[d][m] - pos[d][n];	// Distance between particles in one 
+					// direction
+Dist2 += pow(dist[d][m],2); 		// Distance between particles
+}
+EP += 4 * (pow(Dist2,-6) - pow(Dist2,-3)); 	// Total potential energy
+}				
+}
+}
+
+// ________ Calculate Kinetic Energy of each particle ________ \\
+
+void Calculate_Kinetic_Energy(){
+double Ek[N];
+EK = 0;      				// Reset total kinetic energy parameter
+for (int n = 0; n < N; ++n){
+Ek[n] = 0; 				// Reset kinetic energy parameters
+for (int d = 0; d < dim; ++d)
+Ek[n] += 0.5 * pow(vel[n][d],2); 	// Calculate kinet energy /particle/dim
+EK += Ek[n];
+}
+}
+
+// ________ Calculate Kinetic Energy of each particle________ \\
+
+void Energy_Correction(){
+
+}
 
 /*
 _________________________________________________________
@@ -183,11 +234,15 @@ _________________________________________________________
 // ________ Display values on screen ________ \\
 
 void Display(){
-int n;
-for (n = 0; n < N; ++n)		
-cout << pos[1][n] << " " << endl ;
+if (runtemp > 10){
+runtemp += -10;	
+for (int n = 0; n < N; ++n){
+cout << n << "." << run << " F: " << force[0][n] << " V: " << vel[0][n] << " X: " << pos[0][n] << endl;
 }
-
+cout << "EK: " << EK << " EP: " << EP << " EP + EK: " << EP+EK << endl;
+}
+runtemp += 1;
+}
 
 /*
 _________________________________________________________
@@ -199,13 +254,16 @@ _________________________________________________________
 
 int main(int argc, char*argv[]){
 Parameters(argc, argv);
-cout << "Test" ;
 Make_array(argc, argv);
 Initiate_Position();
 Initiate_Velocity();
-for (int run = 0; run<Run; ++run){
-dt = 0.01;
+for (run = 0; run<Run; ++run){
+Update_force();
+Update_velocity();
 Update_position();
+Calculate_Kinetic_Energy();
+Calculate_Potential_Energy();
+Energy_Correction();
 Display();
 }
 
