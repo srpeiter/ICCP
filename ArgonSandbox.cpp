fb3265a6dd77837b9b124b55f7d6b59aa501cb6f
IRiViI <rckvnk@gmail.com>
2015-02-19 13:39:17 +0000

    bug fixed in the force calculation function

--- ArgonSandbox.cpp
+++ ArgonSandbox.cpp
@@ -33,9 +33,11 @@ double dt;		// discrete time step
 const int dim = 3;	// Number of dimension
 double EK;		// Total kinetic energy
 double EP;		// Total potential energy
-double *pos[dim];	// Position array x,y,z
-double *vel[dim];	// Velocity array x,y,z
-double *force[dim];	// Force array x,y,z
+double Boxlength; 	// The length of the box
+double *pos[dim];	// Position array X,Y,Z
+double *POS[dim];	// Position array X,Y,X with boundaries included
+double *vel[dim];	// Velocity array Vx,Vy,Vz
+double *force[dim];	// Force array Fx,Fy,Fz
 
 double *dist[dim];	// A temperary matrix used for distance calculations
 
@@ -55,10 +57,12 @@ void Initiate_Velocity();
 void Update_position();
 void Update_velocity();
 void Update_force();
+void Update_boundaries();
 void Display();
 void Calculate_Potential_Energy();
 void Calculate_Kinetic_Energy();
 void Energy_Correction();
+
 
 /*
 _________________________________________________________
@@ -90,6 +94,7 @@ pos[i]= new double[N] ();
 vel[i]= new double[N] (); 
 force[i]= new double[N] ();
 dist[i] = new double[N] ();
+POS[i] = new double[27 * N] (); // The positions will be dublicated all around the box
 } 
 // Vector preallocating
 }
@@ -97,8 +102,8 @@ dist[i] = new double[N] ();
 // ________ Process parameters ________ \\
 
 void Parameters(int argc, char* argv[]){
-if (argc < 2){
-printf("Error: Give the number of particles N followed by the number of runs. \nExample: './ArgonSandbox 9 10 0.001', meaning 9 particles and 10 runs 1\n");}
+if (argc < 3){
+printf("Error: Give the number of particles N followed by the number of runs. \nExample: './ArgonSandbox 9 10 ', meaning 9 particles and 10 runs 1\n");}
 else{
 N = atoi(argv[1]);
 Run = atoi(argv[2]); 
@@ -110,7 +115,7 @@ dt = 0.001;
 
 void Initiate_Position(){
 for (int n=0; n<N; ++n){
-pos[0][n] = 1.1 * n;	// Let's start easy
+pos[0][n] = 1.2 * n;	// Let's start easy
 pos[1][n] = 0;
 pos[2][n] = 0;
 }
@@ -139,7 +144,7 @@ _________________________________________________________
 void Update_position(){
 for (int n = 0; n < N; ++n){
 for (int i = 0; i < dim; ++i){		
-pos[i][n] = pos[i][n] + vel[i][n] *dt;		// x(i) = x(i-1) + v(i)*dt
+pos[i][n] = pos[i][n] + vel[i][n] *dt;	// x(i) = x(i-1) + v(i)*dt
 }}
 }
 
@@ -149,7 +154,7 @@ pos[i][n] = pos[i][n] + vel[i][n] *dt;		// x(i) = x(i-1) + v(i)*dt
 void Update_velocity(){
 for (int n = 0; n < N; ++n){
 for (int i = 0; i < dim; ++i){		
-vel[i][n] = vel[i][n] + force[i][n] *dt;	// v(i) = v(i-1) + f(i)*dt
+vel[i][n] = vel[i][n] + force[i][n] *dt;// v(i) = v(i-1) + f(i)*dt
 }}
 }
 
@@ -175,6 +180,20 @@ force[d][n] += 24 * (-2*pow(Dist2,-7) + pow(Dist2,-4)) * dist[d][m];
 } } }
 }
 
+// ________ Update boundaries ________ \\
+
+// copy the particles in the box all around the middle box
+void Update_boundaries(){
+for (int b = 0; b << 27; ++b){ // Box 1 till 27
+
+for (int n = 0; n << N; ++n){
+POS[d][n+(b * N)] = pos[d][n] + Boxlength * Shift_X; 
+POS[d][n+(b * N)] = pos[d][n] + Boxlength * Shift_Y; 
+POS[d][n+(b * N)] = pos[d][n] + Boxlength * Shift_Z; 
+} }
+}
+
+
 /*
 _________________________________________________________
 
@@ -191,7 +210,6 @@ void Calculate_Potential_Energy(){
 double Dist2;
 EP = 0; 				// Reset total potential energy parameter
 for (int n = 0; n < N; ++n){
-for (int d = 0; d < dim; ++d)
 for (int m = n+1; m < N; ++m){		// Do not count double the potential energy
 Dist2 =0;				// Reset distance squared
 for (int d = 0; d < dim; ++d){
@@ -199,25 +217,21 @@ dist[d][m] = pos[d][m] - pos[d][n];	// Distance between particles in one
 					// direction
 Dist2 += pow(dist[d][m],2); 		// Distance between particles
 }
-EP += 4 * (pow(Dist2,-6) - pow(Dist2,-3)); 	// Total potential energy
-}				
-}
+EP += 4 * (pow(Dist2,-6)-pow(Dist2,-3)); // Total potential energy
+} }
 }
 
-// ________ Calculate Kinetic Energy of each particle ________ \\
+// ________ Calculate Kinetic Energy ________ \\
 
 void Calculate_Kinetic_Energy(){
-double Ek[N];
 EK = 0;      				// Reset total kinetic energy parameter
 for (int n = 0; n < N; ++n){
-Ek[n] = 0; 				// Reset kinetic energy parameters
 for (int d = 0; d < dim; ++d)
-Ek[n] += 0.5 * pow(vel[n][d],2); 	// Calculate kinet energy /particle/dim
-EK += Ek[n];
+EK += 0.5 * pow(vel[d][n],2);	 	// Calculate kinet energy /particle/dim
 }
 }
 
-// ________ Calculate Kinetic Energy of each particle________ \\
+// ________ Energy Correction ________ \\
 
 void Energy_Correction(){
 
@@ -234,12 +248,17 @@ _________________________________________________________
 // ________ Display values on screen ________ \\
 
 void Display(){
-if (runtemp > 10){
-runtemp += -10;	
+int EnergyDisp = 1;		// Display energies (1/0)
+int PVFDisp = 0;		// Display position, velocity, force (1/0)
+if (runtemp > 100){		// only display every x runs
+runtemp += -100;			// reset counter
+if (PVFDisp > 0){
 for (int n = 0; n < N; ++n){
 cout << n << "." << run << " F: " << force[0][n] << " V: " << vel[0][n] << " X: " << pos[0][n] << endl;
-}
+} }
+if (EnergyDisp > 0){
 cout << "EK: " << EK << " EP: " << EP << " EP + EK: " << EP+EK << endl;
+}
 }
 runtemp += 1;
 }
