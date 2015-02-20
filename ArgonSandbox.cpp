adfbef0ea3530bc75828ff86956e0b774ec9150d
IRiViI <rckvnk@gmail.com>
2015-02-20 20:59:02 +0000

    boundary conditions added

--- ArgonSandbox.cpp
+++ ArgonSandbox.cpp
@@ -24,9 +24,9 @@ _________________________________________________________
 _________________________________________________________
 
 */
-double a;
+double a;	   	// Length of unit FCC unit cell
 int N;			// Number of particles
-int cub_num;
+int cub_num;		// 
 int Run; 		// Number of simulations rounds
 int run;		// Round number
 int runtemp = 0;	// Used for the display function 
@@ -60,9 +60,12 @@ void Initiate_Position_FCC();
 void Initiate_Velocity();
 void Update_position();
 void Update_velocity();
-void Update_force();
+void Update_position_with_boundaries();
+void Update_force_brute_force();
+void Update_force_with_boundaries_brute_force();
 void Update_boundaries();
 void Display();
+void Matrix_row_checker();
 void Calculate_Potential_Energy();
 void Calculate_Kinetic_Energy();
 void Energy_Correction();
@@ -107,19 +110,19 @@ POS[i] = new double[27 * N] (); // The positions will be dublicated all around t
 
 void Parameters(int argc, char* argv[]){
 if (argc !=4){
-printf("Error: Give the number of particles N followed by the denisty and number of runs. \nExample: './ArgonSandbox 9 10 ', meaning 9 particles and 10 runs 1\n");}
+printf("Error: Give the number of particles N followed by the denisty and number of runs. \nExample: './ArgonSandbox 9 1 10', meaning 9 particles and 10 runs 1\n");}
 else{
+
 // Inputes
 N = atoi(argv[1]);
 density = atof(argv[2]);
 Run = atoi(argv[3]); 
+
 // Others and calculated values
 dt = 0.0001;
 boxlength = pow((N/density),0.3333);
 cub_num = ceil(pow(N,0.333));
 a=boxlength/cub_num;
-
-cout << a << endl;
 }
 }
 
@@ -149,8 +152,6 @@ n++;
 void Initiate_Position_FCC(){
 double unitcell[3][4]={{0, 0.5*a, 0.5*a,0},{0,0,0.5*a ,0.5*a},{0,0.5*a,0,0.5*a}};
 int n = 0;
-
-
 for (int x = 0; x < cub_num; ++x)
 for (int y = 0; y < cub_num; ++y)
 for (int z = 0; z < cub_num; ++z)
@@ -185,9 +186,23 @@ _________________________________________________________
 // ________ Update particles position ________ \\
 
 void Update_position(){
+
+// basic function
+
 for (int n = 0; n < N; ++n){
 for (int i = 0; i < dim; ++i){		
-pos[i][n] = pos[i][n] + vel[i][n] *dt;	// x(i) = x(i-1) + v(i)*dt
+pos[i][n] += pos[i][n] + vel[i][n] *dt;	// x(i) = x(i-1) + v(i)*dt
+}}
+}
+
+void Update_position_with_boundaries(){
+
+// This function puts particles back in the box when they would otherwise leave the box
+
+for (int n = 0; n < N; ++n){
+for (int i = 0; i < dim; ++i){		
+pos[i][n] += vel[i][n] *dt;	// x(i) = x(i-1) + v(i)*dt
+pos[i][n] += -floor(pos[i][n]/boxlength); // subtracts (-1, 0 or 1) * boxlength
 }}
 }
 
@@ -204,7 +219,35 @@ vel[i][n] = vel[i][n] + force[i][n] *dt;// v(i) = v(i-1) + f(i)*dt
 // ________ Update particles force ________ \\
 
 
-void Update_force(){
+void Update_force_with_boundaries_brute_force(){
+
+// This function calculates the force between every particle for each particle including the particles due to the boundary conditions (except itself) 
+
+double Dist2;
+int d;
+for (int n = 0; n < N; ++n){
+for (d = 0; d < dim; ++d)
+force[d][n] = 0; 			// Reset the force
+for (int m = 0; m < 27 * N; ++m){	// Including boundaries, 27 x N particles
+//if (13 * N + n != m){			// Only calculate between different 
+					// particles (mind that we are dealing with 
+					// comparing with the boundary matrix 
+					// (middle box needed)
+Dist2 = 0;				// Reset "Distance squared"
+for (d = 0; d < dim; ++d){
+dist[d][m] = POS[d][m] - pos[d][n];	// Calculate the distance in x,y or z
+Dist2 += pow(dist[d][m],2);		// Calculate the distance squared
+}
+if (Dist2 != 0){
+for (d = 0; d < dim; ++d)		// Calculate the force
+force[d][n] += 24 * (-2*pow(Dist2,-7) + pow(Dist2,-4)) * dist[d][m];
+} } }
+}
+
+void Update_force_brute_force(){
+
+// This function calculates the force between every particle for each particle (except itself) 
+
 double Dist2;
 int d;
 for (int n = 0; n < N; ++n){
@@ -230,15 +273,15 @@ force[d][n] += 24 * (-2*pow(Dist2,-7) + pow(Dist2,-4)) * dist[d][m];
 
 void Update_boundaries(){
 int b = 0;
-for (int Shift_X = -1; Shift_X < 2; ++Shift_X){
-for (int Shift_Y = -1; Shift_Y < 2; ++Shift_Y){
-for (int Shift_Z = -1; Shift_Z < 2; ++Shift_Z){
+for (int x = -1; x < 2; ++x){
+for (int y = -1; y < 2; ++y){
+for (int z = -1; z < 2; ++z){
 for (int n = 0; n < N; ++n){
-POS[0][n+(b * N)] = pos[0][n] + boxlength * Shift_X; 
-POS[1][n+(b * N)] = pos[1][n] + boxlength * Shift_Y; 
-POS[2][n+(b * N)] = pos[2][n] + boxlength * Shift_Z; 
+POS[0][n+(b * N)] = pos[0][n] + boxlength * x; 
+POS[1][n+(b * N)] = pos[1][n] + boxlength * y; 
+POS[2][n+(b * N)] = pos[2][n] + boxlength * z; 
 } 
-b += 1;
+b++;
 } } } 
 }
 
@@ -274,10 +317,9 @@ EP += 4 * (pow(Dist2,-6)-pow(Dist2,-3)); // Total potential energy
 
 void Calculate_Kinetic_Energy(){
 EK = 0;      				// Reset total kinetic energy parameter
-for (int n = 0; n < N; ++n){
+for (int n = 0; n < N; ++n)
 for (int d = 0; d < dim; ++d)
 EK += 0.5 * pow(vel[d][n],2);	 	// Calculate kinet energy /particle/dim
-}
 }
 
 // ________ Energy Correction ________ \\
@@ -304,13 +346,26 @@ runtemp += -0;  		// reset counter
 if (PVFDisp > 0){
 for (int n = 0; n < N; ++n){
 // cout << n << "." << run << " F: " << force[0][n] << " V: " << vel[0][n] << " X: " << pos[0][n] << endl;
-// cout << n << "." << run << " X: " << POS[0][n] << " " << "Y: " << POS[1][n] << " " << "Z: " << POS[2][n] << " " << endl;
 // cout << n << "." << run << " X: " << pos[0][n] << " " << "Y: " << pos[1][n] << " " << "Z: " << pos[2][n] << " " << endl;
+} 
+for (int n = 0; n < 27 * N; ++n){
+// cout << n << "." << run << " X: " << POS[0][n] << " " << "Y: " << POS[1][n] << " " << "Z: " << POS[2][n] << " " << endl;
 } }
 if (EnergyDisp > 0){
 cout << "EK: " << EK << " EP: " << EP << " EP + EK: " << EP+EK << endl;
 } }
 runtemp += 1;
+}
+
+// ________ Check for matrix for rows with the same values ________ \\
+
+void Matrix_row_checker(){
+for (int n = 0; n < 27 * N; ++n)
+for (int m = n + 1; m < 27 * N; ++m)
+if (POS[0][n] == POS[0][m])
+if (POS[1][n] == POS[1][m])
+if (POS[2][n] == POS[2][m])
+cout << n << " " << m << endl;
 }
 
 /*
@@ -329,9 +384,10 @@ Initiate_Velocity();
 for (run = 0; run<Run; ++run){
 Update_boundaries();
 Display();
-Update_force();
+// Update_force_brute_force();
+Update_force_with_boundaries_brute_force();
 Update_velocity();
-Update_position();
+Update_position_with_boundaries();
 Calculate_Kinetic_Energy();
 Calculate_Potential_Energy();
 Energy_Correction();
