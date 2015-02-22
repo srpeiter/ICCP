039778ded04f631a357b79d8a13f2755a0edf8ea
IRiViI <rckvnk@gmail.com>
2015-02-22 21:26:29 +0000

    The simulation works great with boundaries and FCC structure. The function that calculates the potential energy Calculate_Potential_Energy_with_boundaries seems to have some problem but energy seems to act as if it is conserved. ^Calculate_Potential_Energy seems not to suffer from this bug, but the energy is not conserved because it does not take the energy due to the bounaries in consiration.

--- ArgonSandbox.cpp
+++ ArgonSandbox.cpp
@@ -7,11 +7,6 @@
 using namespace std;
 
 /* 
-This is a kind or remake of Argonsimulation.cpp.
-Out of hopelessness, I created a new file that startes simpler such that I can investigate the problem more isolated.
-*/
-
-/* 
 NOTES:
 Life is awesome
 */
@@ -24,25 +19,29 @@ _________________________________________________________
 _________________________________________________________
 
 */
+
+double EK;		// Total kinetic energy
+double EP;		// Total potential energy
+double boxlength; 	// The length of the box
+double dt;		// discrete time step
+double density;		// The denisty of the box
 double a;	   	// Length of unit FCC unit cell
+double kb = 1.38066*pow(10,-23);	// Boltzmann constant
+double Temp;		// Temperature
+
 int N;			// Number of particles
 int cub_num;		// Number of unit FCC boxes
 int Run; 		// Number of simulations rounds
 int run;		// Round number
-int runtemp = 0;	// Used for the display function 
-double dt;		// discrete time step
-double density;		// The denisty of the box
+int runtemp = -1;	// Used for the display function 
 const int dim = 3;	// Number of dimension
-double EK;		// Total kinetic energy
-double EP;		// Total potential energy
-double boxlength; 	// The length of the box
+
 double *pos[dim];	// Position array X,Y,Z
 double *posb[dim];	// Position array X,Y,X with boundaries included
 			// The size of this matrix is 27 * N
 double *vel[dim];	// Velocity array Vx,Vy,Vz
 double *force[dim];	// Force array Fx,Fy,Fz
-double kb = 1.38066*pow(10,-23);	// Boltzmann constant
-double Temp;		// Temperature
+
 
 
 /*
@@ -53,28 +52,36 @@ _________________________________________________________
 
 */
 
+// Initiation Functions
 void Make_array();
 void Parameters();
 void Initiate_Position_Simple();
-void Initiate_Position_Cubic();
+void Initiate_Position_Cubic(); 			// Broken
 void Initiate_Position_FCC();
 void Initiate_Velocity();
+
+// Updating functions
 void Update_position();
 void Update_velocity();
 void Update_position_with_boundaries();
 void Update_force_brute_force();
 void Update_force_with_boundaries_brute_force();
 void Update_boundaries();
-void Display();
-void Matrix_row_checker();
+
+// Energy related functions
 void Calculate_Potential_Energy();
 void Calculate_Potential_Energy_with_boundaries();
 void Calculate_Kinetic_Energy();
-void Energy_Correction();
+void Energy_Correction(); 				// Empty
+
+// Gas quantities
 void Calculate_Temperature();
 void Adjust_Temperature();
-void Clean_up();
 
+// Extra functions
+void Clean_up();
+void Display();
+void Matrix_row_checker();
 /*
 _________________________________________________________
 
@@ -89,7 +96,7 @@ normal_distribution<double> maxwell(0.0,1.0);
 /*
 _________________________________________________________
 
- --------------- Initiating Functions ------------------ 
+ --------------- Initiation Functions ------------------ 
 _________________________________________________________
 
 */
@@ -113,7 +120,7 @@ posb[i] = new double[27 * N] (); // The positions will be duplicated all around 
 
 void Parameters(int argc, char* argv[]){
 if (argc !=5){
-printf("Error: Give the number of particles N followed by the denisty, number of runs and time steps. \nExample: './ArgonSandbox 9 1 10 0.0001', meaning 9 particles and 10 runs with density 1. Taking time steps dt.\n");}
+printf("Error: Give the number of particles N followed by the denisty, number of runs and time steps. \nExample: './ArgonSandbox 9 1 10 0.001', meaning 9 particles and 10 runs with density 1. Taking time steps 0.001.\n");}
 else{
 
 // Inputes
@@ -195,7 +202,6 @@ _________________________________________________________
 // ________ Update particles position ________ \\
 
 
-
 // basic function
 
 void Update_position(){
@@ -230,6 +236,31 @@ vel[i][n] = vel[i][n] + force[i][n] *dt;// v(i) = v(i-1) + f(i)*dt
 
 
 // This function calculates the force between every particle for each 
+// particle (except itself) 
+// Note: "dist" as array is not necessary
+
+void Update_force_brute_force(){
+double Dist2;
+double dist[dim];
+int d;
+for (int n = 0; n < N; ++n){
+for (d = 0; d < dim; ++d)
+force[d][n] = 0; 			// Reset the force
+for (int m = 0; m < N; ++m){
+if (n !=m){				// Only calculate between different 
+					// particles
+Dist2 = 0;				// Reset "Distance squared"
+for (d = 0; d < dim; ++d){
+dist[d] = pos[d][m] - pos[d][n];	// Calculate the distance in x,y or z
+Dist2 += pow(dist[d],2);		// Calculate the distance squared
+}
+for (d = 0; d < dim; ++d){		// Calculate the force
+force[d][n] += 24 * (-2*pow(Dist2,-7) + pow(Dist2,-4)) * dist[d];
+}
+} } }
+}
+
+// This function calculates the force between every particle for each 
 // particle including the particles due to the boundary conditions 
 // (except itself) 
 
@@ -256,32 +287,6 @@ force[d][n] += 24 * (-2*pow(Dist2,-7) + pow(Dist2,-4)) * dist[d];
 } 
 if (Dist2 < 0.4)
 cout << n << " " << m << " " << Dist2 << " " << force[0][1] << endl;
-} } }
-}
-
-
-// This function calculates the force between every particle for each 
-// particle (except itself) 
-// Note: "dist" as array is not necessary
-
-void Update_force_brute_force(){
-double Dist2;
-double dist[dim];
-int d;
-for (int n = 0; n < N; ++n){
-for (d = 0; d < dim; ++d)
-force[d][n] = 0; 			// Reset the force
-for (int m = 0; m < N; ++m){
-if (n !=m){				// Only calculate between different 
-					// particles
-Dist2 = 0;				// Reset "Distance squared"
-for (d = 0; d < dim; ++d){
-dist[d] = pos[d][m] - pos[d][n];	// Calculate the distance in x,y or z
-Dist2 += pow(dist[d],2);		// Calculate the distance squared
-}
-for (d = 0; d < dim; ++d){		// Calculate the force
-force[d][n] += 24 * (-2*pow(Dist2,-7) + pow(Dist2,-4)) * dist[d];
-}
 } } }
 }
 
@@ -342,15 +347,20 @@ double dist[dim];
 EP = 0; 				// Reset total potential energy parameter
 for (int n = 0; n < N; ++n){
 for (int m = 0; m < 27 * N; ++m){	// Compare with the array containin all parti.
+if (13 * N + n != m){			// Only calculate between different 
+					// particles (mind that we are 
+					// comparing with the boundary matrix 
+					// (middle box needed)
 Dist2 =0;				// Reset distance squared
 for (int d = 0; d < dim; ++d){
+
 dist[d] = posb[d][m] - pos[d][n];	// Distance between particles in one 
 					// direction
 Dist2 += pow(dist[d],2); 		// Distance between particles
 }
 EP += 4 * (pow(Dist2,-6)-pow(Dist2,-3))/2; // Total potential energy 
 // Note: now I devide it by 2 because the energy will be counted double because it is harder to keep track whether we are dealing with the same particle
-} }
+} } }
 }
 
 // ________ Calculate Kinetic Energy ________ \\
@@ -420,7 +430,10 @@ delete [] posb[d];
 // ________ Display values on screen ________ \\
 
 void Display(){
-int Display_round = 100;			// Display after "x"  rounds
+int Display_round = 1;			// Display after "x"  rounds
+if (runtemp < 0){
+runtemp = Display_round;		// Allways include the first simulation
+}
 runtemp += 1;
 if (runtemp >= Display_round){		// only display every "x" runs
 runtemp += -Display_round;  		// reset counter
@@ -433,8 +446,8 @@ for (int n = 0; n < 27 * N; ++n){
 // cout << n << "." << run << " X: " << posb[0][n] << " " << "Y: " << posb[1][n] << " " << "Z: " << posb[2][n] << " " << endl;
 }
 cout << "EK: " << EK << " EP: " << EP << " EP + EK: " << EP+EK << endl;
-} 
 // cout << Temp << endl;
+} 
 }
 
 // ________ Check for matrix for rows with the same values ________ \\
@@ -458,25 +471,38 @@ _________________________________________________________
 */
 
 int main(int argc, char*argv[]){
+//--- Read and make variables ---
 Parameters(argc, argv);			// Insert parameters
 Make_array(argc, argv);			// preallocate arrays
+//--- Initiate ---
 Initiate_Position_FCC();		// Initiate positions
 Initiate_Velocity(); 			// Initiate velocity
-for (run = 0; run<Run; ++run){		// Display parameters
+//--- Calculate values with initial conditions ---
+Update_boundaries();			// surround the box with replica boxes
+Calculate_Kinetic_Energy();		// Calculate the kinetic energy
+Calculate_Potential_Energy_with_boundaries(); // Calculate the potential energy
+		// Alternative: Calculate_Potential_Energy();
+for (run = 0; run<Run; ++run){		
+//--- Display values ---
 Display();				// Display parameters
+//--- Update system ---
 Update_boundaries();			// surround the box with replica boxes
 Update_force_with_boundaries_brute_force(); // Update the force between particles
-// Update_force_brute_force();
+		// Alternative: Update_force_brute_force();
 Update_velocity();			// Update the velocity
 Update_position_with_boundaries();	// Update the position
-// Update_position();
+		// Alternative:	Update_position();
+//--- Calculate values ---
 Calculate_Kinetic_Energy();		// Calculate the kinetic energy
-Calculate_Potential_Energy();		// Calculate the potential energy
+			
+//Calculate_Potential_Energy_with_boundaries();
+	// Alternative: Calculate_Potential_Energy();
+Calculate_Potential_Energy();
 Calculate_Temperature();		// Calculate the temperature
-Energy_Correction();			// -- Under construction --
+
 // Adjust_Temperature();		// Change the temperature of the system
 }
-cout << "a: " << a << " boxlength: " << boxlength << " cub_num: " << cub_num << endl;
+
 
 Clean_up();
 }
