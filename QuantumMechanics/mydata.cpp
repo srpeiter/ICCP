f4c32ee0ac7662d18404845ce0cabf76fed89b3c
srpeiter <ps_sarwan@live.nl>
2015-03-08 23:15:16 +0000

    2dplot added

--- QuantumMechanics/mydata.cpp
+++ QuantumMechanics/mydata.cpp
@@ -1,4 +1,31 @@
 
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+int N=20;
+double xmeas[20];
+double ymeas[20];
+double zmeas[20];
+
+
+void maketestdata()
+{
+	for (int i; i<N; i++)
+	{
+	xmeas[i]=i;
+	ymeas[i]=3*i;
+	zmeas[i]=4*i+4;
+
+	}
+
+
+
+}
+
+
+
 class mydata
 {
 int particles;
@@ -6,12 +33,21 @@ double *xdata=NULL;
 double *ydata=NULL;
 double *zdata=NULL;
 public:
-graphdata (double *xdata, int particles) : xdata(xdata), particles(particles) {}
-graphdata (double *xdata, double *ydata, int particles): xdata(xdata), ydata(ydata), particles(particles) {}
-graphdata (double *xdata, double *ydata, double *zdata, int particles): xdata(xdata), ydata(ydata), zdata(zdata), particles(particles) {}
+mydata (double *xdata, int particles) : xdata(xdata), particles(particles) {}
+
+mydata (double *xdata, double *ydata, int particles): xdata(xdata), ydata(ydata), particles(particles) {}
+
+mydata (double *xdata, double *ydata, double *zdata, int particles): xdata(xdata), ydata(ydata), zdata(zdata), particles(particles) {}
+
+
 void printtofile(char filename[]);
+void plot2d();
+void plot3d();
+void histplot();
 };
-void graphdata::printtofile(char filename[])
+
+
+void mydata::printtofile(char filename[])
 {
 FILE *f;
 f=fopen(filename, "w");
@@ -33,3 +69,23 @@ fprintf(f, "%f %f %f\n",xdata[i], ydata[i], zdata[i]);
 }
 
 
+void mydata::plot2d()
+{
+printtofile("test.dat");
+system ("./myplot test.dat using 1:2");
+
+}
+
+
+int main()
+{
+maketestdata();
+
+mydata Testdata(xmeas,ymeas,N);
+
+Testdata.plot2d();
+
+return 0;
+
+
+}
