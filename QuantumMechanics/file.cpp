2d4485f471ecad9fc7c3fc418b02f1dbb95dcb7e
srpeiter <ps_sarwan@live.nl>
2015-03-11 02:16:25 +0000

    plotting functions :2plot , histogram, 3d static plot implemented

--- QuantumMechanics/file.cpp
+++ QuantumMechanics/file.cpp
@@ -1,1 +1,41 @@
 
+#include"plotting.h"
+
+using namespace std;
+const int N=1000;
+double xmeas[N];
+double ymeas[N];
+double zmeas[N];
+
+default_random_engine generator;
+normal_distribution<double> norm_dist(0.0,1.0) ;
+
+
+void maketestdata()
+{
+	for (int i; i<N; i++)
+	{
+	xmeas[i]=norm_dist(generator);
+	ymeas[i]=norm_dist(generator);
+	zmeas[i]=norm_dist(generator);
+
+	}
+
+
+
+}
+
+int main()
+{
+maketestdata();
+
+mydata Testdata(xmeas,N);
+Testdata.printtofile("test.dat");
+
+//Testdata.plot2d();
+Testdata.histplot();
+//Testdata.plot3d();
+return 0;
+
+
+}
---
+++ QuantumMechanics/plot_func.cpp
@@ -0,0 +1,44 @@
+#include"plotting.h"
+
+void mydata::printtofile(char filename[])
+{
+FILE *f;
+f=fopen(filename, "w");
+if (xdata != NULL && ydata == NULL)
+{
+for (int i =0; i < particles; i++)
+fprintf(f, "%f\n",xdata[i]);
+}
+if (xdata != NULL && ydata != NULL && zdata ==NULL)
+{
+for (int i =0; i < particles; i++)
+fprintf(f, "%f %f\n",xdata[i], ydata[i]);
+}
+if (xdata != NULL && ydata != NULL && zdata !=NULL)
+{
+for (int i =0; i < particles; i++)
+fprintf(f, "%f %f %f\n",xdata[i], ydata[i], zdata[i]);
+}
+}
+
+
+void mydata::plot2d()
+{
+printtofile("test.dat");
+system ("./myplot");
+
+}
+
+void mydata::histplot()
+{
+printtofile("test.dat");
+system("./hist");
+}
+
+void mydata::plot3d()
+{
+printtofile("test.dat");
+system("./plot3d");
+
+}
+
