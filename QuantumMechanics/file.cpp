72e5c840edfe37155678c906e9dd6bc9a9d6c2e3
srpeiter <ps_sarwan@live.nl>
2015-03-14 01:28:57 +0000

    everything looks good till now

--- QuantumMechanics/file.cpp
+++ QuantumMechanics/file.cpp
@@ -1,17 +1,24 @@
 #include"plotting.h"
-
+#include<random>
+#include<chrono>
 
 using namespace std;
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+default_random_engine generator(seed);
+uniform_real_distribution<double> distribution(0.0,1.0);
 
 class particle
 {
 
-friend class part_prop;
+friend class observable; 
+ // defining a friend class so to have acces to class particles
 
-private:
+private:		// all variable needed to do computations
+
 double r1[3];
 double r2[3];
 double phi_1L, phi_1R, phi_2L, phi_2R, r_12;
+double phi_1, phi_2, xii , wave_func;
 double alpha=2, a=1 , beta , s;
 
 protected:
@@ -27,25 +34,23 @@ particle( double position[][3], double beta, double s) :  beta(beta), s(s)
   r2[0] = position[1][0];
   r2[1] = position[1][1];
   r2[2] = position[1][2];
-  
-  pre_comp();
 }
 
 ~particle () {};
 
-void pre_comp();
-double phi1();
+void initialize();	// initialize everything
+void phi1();		
 
-double phi2();
+void  phi2();
 
-double xi();
+void xi();
 
-double wavefunction();
+double wavefunction();	// total wavefunction
 
 
 };
 
-void particle::pre_comp()
+void particle::initialize()
 {
 for (int j = 0 ; j < 3 ; j++)
 {
@@ -78,42 +83,148 @@ phi_2R=exp(-r_2R/a);
 }
 
  
-double particle::phi1()
+void particle::phi1()
 {
-return phi_1L + phi_1R;
+phi_1=phi_1L + phi_1R;
 }
 
 
-double particle::phi2()
+void particle::phi2()
 {
-return phi_2L + phi_2R;
+phi_2=phi_2L + phi_2R;
 }
 
-double particle::xi()
+void particle::xi()
 {
-return exp(r_12/ (alpha*(1+beta*r_12)));
+ xii=exp(r_12/ (alpha*(1+beta*r_12)));
 }
 
 double particle::wavefunction()
 {
-return phi1() * phi2() * xi();
+initialize();
+phi1();
+phi2();
+xi();
+return wave_func= phi_1*phi_2*xii;
 }
 
 
+class observable
+{
+private:
+particle temp;
+double norm_wave=0;
+int N;
+
+public:
+observable(particle& temp, int iterations) : temp(temp), N(iterations) {};
+
+double energy();	
+
+void norm();
+
+double final_energy();
+
+};
+
+double observable::energy()
+{// here we calculate the enery of the particle
+const int dim= 3;
+double first_term, second_term, third_term, fourth_term, fifth_term, sixth_term;
+double inter_res1, inter_res2;
+double energy;
+
+temp.wavefunction();
+
+first_term = 1.0/(temp.a*temp.a);
+second_term = ((temp.phi_1L/temp.r_1L) + (temp.phi_1R/temp.r_1R))* (1.0/temp.a*temp.phi_1);
+third_term =  ((temp.phi_2L/temp.r_2L) + (temp.phi_2R/temp.r_2R))* (1.0/temp.a*temp.phi_2);
+fourth_term = (1.0/temp.r_1L) +  (1.0/temp.r_1R) +  (1.0/temp.r_2L) +  (1.0/temp.r_2R);
+fifth_term = 1.0/temp.r_12;
+
+for(int j=0; j < dim; j++)
+{
+inter_res1=((temp.phi_1L*temp.r_1Lvec[j] + temp.phi_1*temp.r_1Rvec[j])/temp.phi_1) - 
+((temp.phi_2L*temp.r_2Lvec[j] + temp.phi_2*temp.r_2Rvec[j])/temp.phi_2) ;
+
+inter_res2= temp.r_12vec[j]/(2*temp.a*(1+temp.beta*temp.r_12)*(1+temp.beta*temp.r_12));
+
+fifth_term += inter_res1*inter_res2;
+}
+
+sixth_term= ((4*temp.beta +1)*temp.r_12 +4)/(4.0*pow((1+temp.beta*temp.r_12),4)*temp.r_12);
+
+energy= -first_term + second_term + third_term - fourth_term + fifth_term -sixth_term ; 
+
+return energy;
+
+}
+
+void observable::norm()
+{//here we normalize the wavefunction only once per s and beta
+double wave_sq=0, temp_wave;
+
+for (int j=0; j < N ; j++){
+
+temp.r1[0]=distribution(generator);
+temp.r1[1]=distribution(generator);
+temp.r1[2]=distribution(generator);
+temp.r2[0]=distribution(generator);
+temp.r2[1]=distribution(generator);
+temp.r2[2]=distribution(generator);
+
+temp_wave= temp.wavefunction();
+wave_sq += temp_wave * temp_wave;
+}
+norm_wave = (1.0/N)* wave_sq;
+fprintf(stdout, "the norm of the wave_func is %f\n",norm_wave);
+}
+
+double observable::final_energy()
+{
+double omega, wave_sq, epsil, fin_energy, av_energy;
+
+norm();
+for (int j=0; j < N ; j++)
+{
+temp.r1[0]=distribution(generator);
+temp.r1[1]=distribution(generator);
+temp.r1[2]=distribution(generator);
+temp.r2[0]=distribution(generator);
+temp.r2[1]=distribution(generator);
+temp.r2[2]=distribution(generator);
+
+wave_sq= temp.wavefunction();
+omega= wave_sq*wave_sq/(norm_wave);
+epsil= energy();
+
+fin_energy += omega*epsil;
+}
+return av_energy = (1.0/N)*fin_energy;
+}
 
 int main(void)
 
 {
- double pos[2][3]= {{0.1,0.2,0.3},{0.5,0.3,0.2}};
+ double pos[2][3]= {{0.3,0.2,0.5},{0.5,0.1,0.1}};
 
-particle electron(pos,0.2, 1.2);
+particle electron(pos,0.3, 1.2);
 
-double wave=electron.wavefunction();
+//double wave=electron.wavefunction();
+observable energy_part(electron, 100);
+//fprintf(stdout, "the wavefunction is %f \n", wave);
+double energy=energy_part.energy();
 
-fprintf(stdout, "the wavefunction is %f \n", wave);
-cout << "wave is "<< wave <<endl;
+energy_part.norm();
+double en= energy_part.final_energy();
+cout << "average energy is "<< en<<endl;
 return 0;
 
 }
 
+
+//double s=1.2;
+//double beta= 0.3;
+//double r1[3]= {0.3, 0.2, 0.5};
+//double r2[3]= {0.5 ,0.1 , 0.1};
 
