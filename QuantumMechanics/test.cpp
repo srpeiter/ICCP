18c828a037e6c14afe21cbffbf256cacfa7ddc1c
mgely <mario.f.gely@gmail.com>
2015-03-23 20:19:02 +0100

    try and use this main function from now on

--- QuantumMechanics/test.cpp
+++ QuantumMechanics/test.cpp
@@ -5,40 +5,42 @@ using namespace std;
 int main(void)
 
 {
- double pos[2][3]= {{0.1,0.23,0.1},{0.5,0.1,0.1}};
-double *out;
-double s=0.1;
-const int N=30;
-double dat1[N], dat2[N];
-double step =0.2;
-int it = 100000;
-particle electron(pos,2.1,0.5,it);
-double an=electron.wavefunction();
-electron.generate_metropolis(step);
+////////////////  PARAMETERS   /////////////////
+
+double metro_step = 0.1;
+double s_min = 0.1;
+double s_max = 3;
+double s_step = 0.01;
+int it = 100000; // Number of iterations in the Monte Carlo 
+double beta_0 = 8.8; // Begining values for the minimiztion algorithm
+double beta_1 = 9;
+double conv_indicator = 0.1; // The minimization algorithm stops when the difference between two successive values is lower
+double eps = 0.1*beta_0; // Speed of convergeance in minimization
+
+////////////////   ALGORITHM   /////////////////
+
+int N = (int)((s_max - s_min)/s_step); // Number of values of s
+double pos[2][3]= {{1,1,1},{1,1,1}}; // Initial position of electrons
+double *out; // Used to store the minimization results: the minimal energy and the beta at which this is achieved
+double dat1[N], dat2[N]; // Stores the s values and the energy values respecfully for plotting
+particle electron(pos,2.1,0.5,it);	// Don't care about the arguments, just to initialize
+electron.wavefunction(); // Initializes the variables in electron
+
+electron.generate_metropolis(metro_step);
 observable energy_part(electron);
 
-//double wave=electron.wavefunction();
-//fprintf(stdout, "the wavefunction is %f \n", wave);
-//double energy=energy_part.comp_integral(0.4,1);
-//fprintf(stdout, "the energy is %f \n", energy);
-
-//energy_part.norm();
-//double en= energy_part.comp_integral(0.9,0.5);
-
-
-for (int j=0 ; j < N; j++)
+for (int j=0 ; j <= N+1; j++)
 {
-out = minimize(energy_part, 8.8, 9,0.1,(s+0.1*j));
-cout << "beta is "<< out[0]<< " and energy is "<<out[1]<< endl;
-//cout << "average energy is "<< en<<endl;
-dat1[j]=(s+0.1*j);
-dat2[j]=out[1];
+	out = minimize(energy_part, beta_0, beta_1, eps,(s_min+s_step*j)); 
+	dat1[j]=(s_min+s_step*j);
+	dat2[j]=out[1];
 }
+
+
+//////////////////    RESULTS     //////////////////////
 
 mydata testclass(dat1, dat2, N);
 testclass.plot2d();
 return 0;
-
-
 }
 
