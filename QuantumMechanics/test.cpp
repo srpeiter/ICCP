26d75cebcb662c03d6cdbcc0dd61c8b50557a49f
mgely <mario.f.gely@gmail.com>
2015-03-26 15:45:02 +0100

    minor changes

--- QuantumMechanics/test.cpp
+++ QuantumMechanics/test.cpp
@@ -5,34 +5,23 @@ using namespace std;
 int main(void)
 
 {
- double initposx= 0.3;
+double initposx= 0.3;
 double out;
 double a=0.1;
-const int N=30;
+const int N=30; 
 double dat1[N], dat2[N];
-double step =0.2;
-int it = 100000;
+int it = 10000;
+double step = 0.2;
 particle electron(initposx, 0.1, it);
 double an=electron.wavefunction();
 electron.generate_metropolis(step);
 observable energy_part(electron);
 
-//double wave=electron.wavefunction();
-//fprintf(stdout, "the wavefunction is %f \n", wave);
-//double energy=energy_part.comp_integral(0.4,1);
-//fprintf(stdout, "the energy is %f \n", energy);
-
-//energy_part.norm();
-//double en= energy_part.comp_integral(0.9,0.5);
-
-
-for (int j=0 ; j < N; j++)
-{
-//out = minimize(energy_part, 2.3, 2.5,0.1,(+0.2*j));
-out = energy_part.comp_integral(a+0.1*j);
-cout << "a is "<< a<< "and energy is "<<  out << endl;//cout << "average energy is "<< en<<endl;
-dat1[j]=(a+0.1*j);
-dat2[j]=out;
+for (int j=0 ; j < N; j++){
+	out = energy_part.comp_integral(a+0.1*j);
+	cout << "a is "<< a<< " and energy is "<<  out << endl;//cout << "average energy is "<< en<<endl;
+	dat1[j]=(a+0.1*j);
+	dat2[j]=out;
 }
 
 double *out2;
